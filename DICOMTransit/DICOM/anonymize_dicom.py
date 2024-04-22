dba91bcfff04ddaae0ec3c7496afe6604145c237
Jhil Patel <jhilpatel13@gmail.com>
2024-04-22 16:56:18 +0000

    Added Log system and improved terminal prints

--- DICOMTransit/DICOM/anonymize_dicom.py
+++ DICOMTransit/DICOM/anonymize_dicom.py
@@ -1,10 +1,11 @@
+import datetime
 import pydicom
 import DICOMTransit.DICOM.anonymize
 import DICOMTransit.DICOM.API
 from pathlib import Path
 
 def anonymize_dicom():
-    
+
     #Asks user for folder path
     folder_path = input("Enter the folder path containing DICOM files: \n")
 
@@ -25,31 +26,28 @@ def anonymize_dicom():
     #Asks user for new ID for Patient Name and Patient ID
     new_ID = input("Enter the new ID for Patient Name and Patient ID: \n")
     
-    #Displays each patient information found at folder path  before anonymization (currently not used because it floods the terminal)
-    '''
-    print("Patient information before anonymization:")
-    for file_path in folder_path.rglob("*.dcm"):
-        try:
-            read = pydicom.dcmread(file_path)
-            print(f"File: {file_path}")
-            print(f"Patient Name: {read.PatientName}")
-            print(f"Patient ID: {read.PatientName}")
-        except Exception as e:
-            print("Failed to read DICOM file {file_path}: {e}")
-    '''
-    #Anonymizes the files
+    #Anonymizes the files with provided ID
     DICOMTransit.DICOM.anonymize.DICOM_anonymize.filelist(files, new_ID)
     
-    #Displays each patient information found at folder path after anonymization (currently not used because it floods the terminal)
-    '''
-    print("Patient information before anonymization:")
-    for file_path in folder_path.rglob("*.dcm"):
-        try:
-            read = pydicom.dcmread(file_path)
-            print(f"File: {file_path}")                
-            print(f"Patient Name: {read.PatientName}")
-            print(f"Patient ID: {read.PatientName}")
-        except Exception as e:
-            print("Failed to read DICOM file {file_path}: {e}")
-    '''
-    print("\n***** ANONYMIZATION COMPLETE *****\n")
+    #Creates log directory if none existant
+    log_directory = Path("Anonymization_Logs")
+    log_directory.mkdir(exist_ok=True)
+    
+    #Creates log file with each DICOM files modified from folder path and saves it at log directory
+    print(f"\nCreating log file...\n")
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    log_file_path = log_directory / f"{current_time}_{folder_path.stem}_log.txt" 
+    with open (log_file_path, "a") as log_file:
+        log_file.write(f"Anonymization Timestamp: {current_time}\n\n")
+        for file_path in folder_path.rglob("*.dcm"):
+            try:
+                read = pydicom.dcmread(file_path)
+                log_file.write(f"Anonymization Log for file: {file_path}\n")
+                log_file.write(f"   Patient Name: {read.PatientName}\n")
+                log_file.write(f"   Patient ID: {read.PatientID}\n")
+            except Exception as e:
+                log_file.write(f"Error processing DICOM file: {file_path}: {e}\n")
+                print(f"Error: Failed to read DICOM file {file_path}: {e}\n")
+
+    print(f"Log file saved to {log_file_path}\n")
+    print("Anonymization Complete!\n")
